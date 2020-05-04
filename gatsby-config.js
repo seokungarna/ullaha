d2eb5c410bea33dff0f07ccccdb7b7bbcf261c39
carnalito80 <kunbg_jk@hotmail.com>
2020-05-04 16:28:58 +0200

    3d com

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,12 +76,12 @@ module.exports = {
         purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: ``,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: ``,
+      },
+    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
